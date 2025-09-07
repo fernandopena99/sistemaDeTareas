c3d3956b16e3934071af460fb88e1e4b1c2dffd8
Fernando <fernando.p99.h@gmail.com>
2025-09-07 16:07:24 -0300

    Proyecto completo CRUD

--- src/main/java/fer/tareas/TareasApplication.java
+++ src/main/java/fer/tareas/TareasApplication.java
@@ -1,13 +1,16 @@
 package fer.tareas;
 
+import fer.tareas.presentacion.SistemasTareasFx;
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TareasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TareasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        //SpringApplication.run(TareasApplication.class, args);
+        Application.launch(SistemasTareasFx.class, args);
+    }
 
 }
---
+++ src/main/java/fer/tareas/repositorio/TareaRepositorio.java
@@ -0,0 +1,7 @@
+package fer.tareas.repositorio;
+
+import fer.tareas.modelo.Tarea;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TareaRepositorio extends JpaRepository<Tarea, Integer> {
+}
